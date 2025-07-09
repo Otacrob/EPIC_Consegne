0b8d865d89ed714d30ebfe950088b54b3c3ab5b0
Enrico Favaro <efavaro96@gmail.com>
2025-07-09 08:19:31 -0400

    Add files via upload

---
+++ UNIT1/S2/Pratica S2_L3.py
@@ -0,0 +1,19 @@
+
+
+while True:
+
+    domanda = input("Ciao, oggi proveremo a scegliere un nome per la tua band musicale! Sei pronto? ")
+
+    if domanda.lower() == "si":
+
+        città = input("Scrivimi il nome della tua città natale!: ")
+        animale = input("Ora dimmi il nome di uno dei tuoi animali domestici!: ")
+        risultato = f"Il nome per la tua band sarà: {città}{animale}"
+        print(risultato)
+        break
+
+    else:
+        print("Non accetto un no come risposta! :) Dovrai dirmi per forza Si")
+
+
+
